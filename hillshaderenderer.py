9c5d4b21f9c24cbc8db70d88275967fa2dbff7e1
AsgerPetersen <asgerpetersen@gmail.com>
2016-05-25 11:21:48 +0200

    Create QImage from numpy and set it on output QgsRasterBlock

--- hillshaderenderer.py
+++ hillshaderenderer.py
@@ -3,7 +3,7 @@
 from qgis.core import *
 from qgis.utils import iface
 import numpy as np
-from PyQt4.QtGui import qRgb
+from PyQt4.QtGui import QImage
     
 class HillshadeRenderer ( QgsRasterRenderer ):
     def __init__( self ):
@@ -13,16 +13,18 @@ class HillshadeRenderer ( QgsRasterRenderer ):
         self.altitude = 45        # in degrees
 
     def block ( self, bandNo, extent, width, height ):
-        self._log("block() called")
+        #self._log("block() called")
+        
         resolution = extent.width() / float(width)
-        self._log("Resolution: " + str(resolution))
+
+        #self._log("Resolution: " + str(resolution))
         block = self.input().block ( 1, extent, width, height )
         data = self._block2numpy(block)
         #self._log(data)
         shaded = self._hillshade(data, self.azimuth, self.altitude, resolution, self.zfactor)
         #self._log(shaded)
         output = self._numpy2block(shaded)
-        self._log(output)
+        #self._log(output)
         return output
         
     def clone(self):
@@ -37,21 +39,21 @@ class HillshadeRenderer ( QgsRasterRenderer ):
         return nparr
         
     def _numpy2block(self, nparray):
-        self._log("numpy2block called")
         height, width = nparray.shape
+        bgra = np.empty((height, width, 4) ,np.uint8, 'C')
+        bgra[:,:,0] = nparray
+        bgra[:,:,1] = nparray
+        bgra[:,:,2] = nparray
+        bgra[:,:,3] = 255
+        img = QImage(bgra.data, width, height, QImage.Format_ARGB32_Premultiplied)
+        img.ndarray = bgra # Need to carry reference or python will GC the memory 
         block = QgsRasterBlock ( QGis.ARGB32_Premultiplied, width, height, 0 )
-        self._log(block)
-        self._log(nparray.shape)
-
-        for r in xrange(height):
-            for c in xrange(width):
-                value = int(nparray[r,c])
-                block.setColor(r, c, qRgb( value, value, value ) )
+        block.setImage(img)
         return block
         
     # http://geoexamples.blogspot.dk/2014/03/shaded-relief-images-using-gdal-python.html
     def _hillshade(self, array, azimuth, angle_altitude, resolution = 1, zfactor = 1):  
-        self._log( "hillshade called")
+        #self._log( "hillshade called")
         f = float(resolution) / zfactor
         x, y = np.gradient(array, f, f)
         slope = np.pi/2. - np.arctan(np.sqrt(x*x + y*y) )  
@@ -63,7 +65,7 @@ class HillshadeRenderer ( QgsRasterRenderer ):
         shaded = np.sin(altituderad) * np.sin(slope)\
                + np.cos(altituderad) * np.cos(slope)\
                * np.cos(azimuthrad - aspect)  
-        return 255*(shaded + 1)/2 
+        return (255*(shaded + 1)/2).astype(np.uint8)
         
     def _log(self, message):
         QgsMessageLog.logMessage(str(message), 'HillshadeRenderer', QgsMessageLog.INFO)
